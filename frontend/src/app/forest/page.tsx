3a8778790d16adbb73af75c2fa576c4843f4d5f6
kristiannorth <kristian.north@gmail.com>
2025-08-11 17:09:01 -0400

    changes to forest, shore, hill

--- frontend/src/app/forest/page.tsx
+++ frontend/src/app/forest/page.tsx
@@ -21,7 +21,7 @@ export default function ForestPage() {
       </div>
 
       <p className="mb-4">
-        The Acoustic Niche Hypothesis (Krause, 1993), proposes an autopoetic soundscape where species evolve to emit and perceive sonic signals in unique spectral niches. Human hearing is also described by this model, suggesting the tandem evolution of the biological structures for both hearing and vocal production for belonging in the biophony.
+        The Acoustic Niche Hypothesis (Krause, 1993), proposes an autopoetic soundscape where species evolve to emit and perceive sonic signals in unique spectral niches. Human hearing is also described by this model, suggesting the tandem evolution of the biological structures for both hearing and vocal production.
       </p>
       
       <div className="flex justify-center my-4">
@@ -35,7 +35,7 @@ export default function ForestPage() {
       </div>
 
       <p className="mb-4">
-        For researchers, decoding these complex communication networks has so far relied on a synthesis of ear training and technical expertises; situated listening, field recording, visual analysis of spectrograms, audio processing, and so forth. Critically, the computer's role was one of translation not interpretation.
+        Decoding these complex communication networks has so far relied on a synthesis of ear training and technical expertises; situated listening, field recording, visual analysis of spectrograms, audio processing... Critically, the computer's role was one of translation not interpretation.
       </p>
 
       <div className="flex justify-center my-4">
@@ -49,7 +49,7 @@ export default function ForestPage() {
       </div>
 
       <p>
-        But the emergence of machine listening via neural networks like BirdNET marks a fundamental shift, automating the interpretive act itself, yielding probabilistic divinations in place of human experts.
+        The emergence of machine listening via neural networks like BirdNET marks a fundamental shift, automating the interpretive act itself, yielding probabilistic divinations in place of human experts.
       </p>
 
       {/* New "Continue" Link */}
